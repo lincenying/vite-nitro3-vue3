226c8e29abb878b6c86cfdb5fc74a99bc3196560
LinCenYing <lincenying@qq.com>
2026-07-03 09:32:20 +0800

    chore: update

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,4 +1,4 @@
-import { webConfig } from '@lincy/unocss-base-config'
+import { breakpoints, webConfig } from '@lincy/unocss-base-config'
 
 const baseConfig = webConfig('wind3', { preflight: 'on-demand' })
 
@@ -10,7 +10,7 @@ export default {
     ],
     theme: {
         breakpoints: {
-            m1440: '1440px',
+            ...breakpoints,
             m1360: '1360.1px',
         },
     },
